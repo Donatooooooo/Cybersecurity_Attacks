ed39a995da17d2674f403f7430bca01b4bd06434
Donatooooooo <donatoboccuzzi@gmail.com>
2024-05-16 19:01:41 +0200

    changed Prolog things

--- analizer.py
+++ analizer.py
@@ -1,40 +1,16 @@
 from Dataset.dataset import Dataset
 from preprocessor import emptyValues
-import pandas 
+from pyswip import Prolog
+import pandas, sys, os
 
-def write_prolog_fact(record):
-    fields = record.split(',')
-    fact = "network_event("
-    for field in fields:
-        if field.isdigit():
-            fact += field + ","
-        elif field.replace('.', '', 1).isdigit():
-            fact += field + ","
-        else:
-            fact += "'" + field + "',"
-    fact = fact[:-1] + ")."
-    return fact
-
-def factWriter():
+def Preprocessor():
     dataset = Dataset('Dataset/cybersecurity_attacks.csv')
-    dataset.dropDatasetColumns([
-        'Source IP Address', 'Timestamp', 'Destination IP Address', 
-        'Payload Data', 'Attack Signature', 'User Information', 
-        'Network Segment', 'Geo-location Data', 'Device Information', 
-        'Timestamp','Source Port','Destination Port', 'Packet Length', 
-        'Severity Level', 'Log Source'
-    ])
-    
+    dataset.dropDatasetColumns(['Source IP Address', 'Timestamp', 'Destination IP Address', 'Payload Data', 
+                                    'Attack Signature', 'User Information', 'Network Segment', 'Geo-location Data',
+                                         'Device Information', 'Timestamp','Source Port','Destination Port',
+                                            'Packet Length', 'Severity Level','Log Source', 'Firewall Logs'])
     dataset = emptyValues(dataset)
     dataset = dataset.getDataset()
     dataset['Proxy Information'] = dataset['Proxy Information'].apply(lambda x: 1 if pandas.notna(x) else 0)
+    return dataset
 
-    
-    with open('Prolog/kb.pl', 'w') as file:
-        for index, row in dataset.iterrows():
-            record = ','.join(map(str, row.tolist()))
-            fact = write_prolog_fact(record)
-            file.write(fact + '\n')
-
-# Esegui la funzione factWriter per scrivere i fatti nel file Prolog
-factWriter()
