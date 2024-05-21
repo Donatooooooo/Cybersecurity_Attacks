231c0b1cba8b789d5d0dcd3a6ff8ef5729fe2ab2
Martycap <mcapone977@gmail.com>
2024-05-21 12:20:52 +0200

    Modified writer

--- Prolog/writer.py
+++ Prolog/writer.py
@@ -1,2 +1,18 @@
-import random
+from pyswip import Prolog
+import csv
 
+prolog = Prolog()
+
+unique_data = set()
+
+with open('Dataset/cybersecurity_attacks.csv', 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    for row in csv_reader:
+        data_tuple = (row['Protocol'], row['Traffic Type'], row['Attack Type'])
+        unique_data.add(data_tuple)
+
+
+with open('protocols_involved.pl', 'w') as pl_file:
+    for data in unique_data:
+        pl_file.write(f"protocols_involved('{data[0]}', '{data[1]}', '{data[2]}').\n")
