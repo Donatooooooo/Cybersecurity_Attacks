e56512c5cc3e8358baad27cb0445761fd39800c4
Martycap <mcapone977@gmail.com>
2024-05-21 15:35:10 +0200

    Added action_taken

---
+++ Prolog/action_taken.pl
@@ -0,0 +1,6 @@
+action_taken('Data', 'Ignored').
+action_taken('Control', 'Ignored').
+action_taken('Control', 'Blocked').
+action_taken('Data', 'Blocked').
+action_taken('Data', 'Logged').
+action_taken('Control', 'Logged').
