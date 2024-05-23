9b81e53b15b325ccd8a2daa6e0d17d8e2ab2a28c
donatooooooo <donatoboccuzzi@gmail.com>
2024-05-23 22:54:10 +0200

    deletted files

--- Prolog/action_taken.pl
+++
@@ -1,6 +0,0 @@
-action_taken('Data', 'Ignored').
-action_taken('Control', 'Ignored').
-action_taken('Control', 'Blocked').
-action_taken('Data', 'Blocked').
-action_taken('Data', 'Logged').
-action_taken('Control', 'Logged').
