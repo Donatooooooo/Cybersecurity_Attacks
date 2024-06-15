fb47786db8a7e29164cd30f8c61df430192bd76f
Martycap <mcapone977@gmail.com>
2024-06-15 19:04:51 +0200

    modified main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Dataset.dataset import Dataset
 from KnowledgeBase.prologManager import PrologManager
+from classifier import classifier
+from regression import regression
 from preprocessor import *
-from kmeans import kMeans
 
 """
 MAINTEST
@@ -16,9 +17,18 @@ dataset.addDatasetColumn("Basescore", prolog.getBasescore())
 dataset = datasetPreprocessor(dataset)
 dataset.saveDataset("Dataset/Altered_cybersecurity_attacks.csv")
 
+
+
 # dataset = Dataset("Dataset/Altered_cybersecurity_attacks.csv")
 # features = dataset.getDataFrame(['Malware Indicators', 'Anomaly Scores', 'Alerts/Warnings', 'Severity Level_Low',
 #                                     'Severity Level_Medium','Attack Type_Intrusion', 'Attack Type_Malware', 'Traffic Type_FTP', 'Traffic Type_HTTP'])
 # kmeans = kMeans().clustering(features, "Attack")
 # dataset.addDatasetColumn('Own Cluster', kmeans.fit_predict(features))
 # dataset.saveDataset("Dataset/Clusterized_dataset.csv")
+
+
+#Classifier
+classifier()
+
+#Regression
+regression()
