23f723879652737dd8a6570d5b9dbf55aed8aac5
donatooooooo <donatoboccuzzi@gmail.com>
2024-06-17 14:56:56 +0200

    modified main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Dataset.dataset import Dataset
-from KnowledgeBase.prologManager import PrologManager
+from KnowledgeBase.kb import KnowledgeBase
 from classifier import ModelTrainerClass
 from classifier_MLP import ModelTrainer
 from preprocessor import prologPreprocessor
@@ -10,21 +10,22 @@ MAINTEST
 """
 dataset = Dataset("Dataset/cybersecurity_attacks.csv")
 
+print("\n\n *** COMPUTING BASESCORE *** \n\n")
 plFrame = prologPreprocessor()
-prolog = PrologManager("KnowledgeBase/rules.pl")
-prolog.computeBasescore(plFrame)
+kb = KnowledgeBase("KnowledgeBase/rules.pl")
+kb.computeBasescore(plFrame)
 
-dataset.addDatasetColumn("Basescore", prolog.getBasescore())
+dataset.addDatasetColumn("Basescore", kb.getBasescore())
 dataset.saveDataset("Dataset/BaseScore_cybersecurity_attacks.csv")
 
-#Classifier
+print("\n\n *** CLASSIFIERS *** \n\n")
 trainer = ModelTrainerClass("Dataset/BaseScore_cybersecurity_attacks.csv", 'Protocol', ['Protocol'])
 trainer_MLP = ModelTrainer("Dataset/BaseScore_cybersecurity_attacks.csv", 'Protocol', ['Protocol'])
 X, y = trainer_MLP.load_and_preprocess_data()
 trainer_MLP.train_model_MLP(X, y, 'MLP')
 trainer.run()
 
-#Regression
+print("\n\n *** REGRESSION *** \n\n")
 trainer = RegressionModelTrainer("Dataset/BaseScore_cybersecurity_attacks.csv", 'Basescore', ['Basescore'])
 X_train, X_test, y_train, y_test = trainer.load_and_preprocess_data()
 model = trainer.build_model(X_train.shape[1])
