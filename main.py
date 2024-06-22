667434dd31990c17fbe6af021a84ed89a38226bb
Martycap <mcapone977@gmail.com>
2024-06-22 02:20:48 +0200

    updated main

--- main.py
+++ main.py
@@ -48,11 +48,15 @@ trainer_MLP = ModelTrainer("Dataset/BaseScore_cybersecurity_attacks.csv", 'Proto
 X, y = trainer_MLP.load_and_preprocess_data()
 trainer_MLP.train_model_MLP(X, y, 'MLP')
 trainer.run()
+additional_features=['Attack Type_Intrusion', 'Attack Type_Malware', 'Malware Indicators', 'Anomaly Scores', 'Alerts/Warnings', 'IDS/IPS Alerts', 'Proxy Information', 'Firewall Logs', 'Packet Type_Data', 'Action Taken_Ignored', 'Action Taken_Logged', 'Traffic Type_FTP', 'Traffic Type_HTTP', 'Log Source_Server']
+trainer_KNN = ModelTrainerClass("Dataset/BaseScore_cybersecurity_attacks.csv", 'Protocol', ['Protocol'],additional_features=additional_features)
+trainer_KNN.train_model_KNN('KNN')
+
 
 print("\n\n *** REGRESSION MODULE *** \n\n")
-trainer = RegressionModelTrainer("Dataset/BaseScore_cybersecurity_attacks.csv", 'Basescore', ['Basescore'])
-X_train, X_test, y_train, y_test = trainer.load_and_preprocess_data()
-model = trainer.build_model(X_train.shape[1])
-trainer.train_model(model, X_train, y_train)
-trainer.evaluate_model(model, X_test, y_test)
+trainer_regressor = RegressionModelTrainer("Dataset/BaseScore_cybersecurity_attacks.csv", 'Basescore', ['Basescore'])
+X_train, X_test, y_train, y_test = trainer_regressor.load_and_preprocess_data()
+model = trainer_regressor.build_model(X_train.shape[1])
+trainer_regressor.train_model(model, X_train, y_train)
+trainer_regressor.evaluate_model(model, X_test, y_test)
 
